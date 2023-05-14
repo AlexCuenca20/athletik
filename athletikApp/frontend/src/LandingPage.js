4db56198b2d9049e15e571d8f244c0021a198059
Alex <alexcuencazurita@gmail.com>
2023-05-14 20:30:26 +0200

    feat: Use React Native Elements in buttons

--- athletikApp/frontend/src/LandingPage.js
+++ athletikApp/frontend/src/LandingPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { TouchableOpacity, StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, View, Image } from 'react-native'
+import { Button } from '@rneui/themed';
 
 export class LandingPage extends Component {
     render() {
@@ -9,15 +10,32 @@ export class LandingPage extends Component {
                     <Image style={styles.image} source={require('../assets/logo.png')} />
                 </View>
                 <View style={styles.lowerContainer}>
-                    <TouchableOpacity style={styles.signUpBtn}>
-                        <Text style={styles.signUpText}>EMPEZAR</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={styles.loginBtn}
+                    <Button
+                        buttonStyle={{ backgroundColor: '#000' }}
+                        containerStyle={{ width: '90%' }}
+                        title="EMPEZAR"
+                        titleStyle={{ fontSize: '10px', fontWeight: 'bold' }}
+                        icon={{
+                            name: 'arrow-right',
+                            type: 'font-awesome',
+                            size: 15,
+                            color: 'white',
+                        }}
+                        iconRight
+                        onPress={() => this.props.navigation.navigate('SignUp')}>
+                    </Button>
+                    <Button
+                        title="¿YA TIENES UNA CUENTA? INICIA SESIÓN"
+                        titleStyle={{
+                            fontSize: '12px',
+                            fontWeight: 'bold',
+                            textDecorationLine: 'underline',
+                            color: '#000'
+                        }}
+                        type="clear"
                         onPress={() => this.props.navigation.navigate('Login')}
                     >
-                        <Text style={styles.loginText}>¿YA TIENES UNA CUENTA? INICIA SESIÓN</Text>
-                    </TouchableOpacity>
+                    </Button>
                 </View>
             </View >
 
@@ -35,45 +53,14 @@ const styles = StyleSheet.create({
     },
     upperContainer: {
         flex: 8,
-        width: '100%',
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#aaa',
     },
     lowerContainer: {
         flex: 2,
-        width: '100%',
         alignItems: 'center',
         justifyContent: 'center',
     },
     image: { width: 220, height: 220 },
-    signUpBtn: {
-        width: "90%",
-        height: 40,
-        margin: 5,
-        backgroundColor: "#DAF7A6",
-        justifyContent: "center",
-        backgroundColor: '#000'
-    },
-    loginBtn: {
-        width: "90%",
-        height: 40,
-        justifyContent: "center",
-        backgroundColor: "#FFFFFF",
-    },
-    loginText: {
-        margin: 5,
-        fontFamily: 'Helvetica',
-        fontWeight: 'bold',
-        fontSize: '10px',
-        textDecorationLine: 'underline'
-    },
-    signUpText: {
-        margin: 5,
-        marginLeft: 15,
-        fontFamily: 'Helvetica',
-        fontWeight: 'bold',
-        fontSize: '12px',
-        color: '#fff'
-    },
 })
